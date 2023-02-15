e9f4dd6f53d2e81ed4290a273eb5ffc38129d2f0
supriyacode17 <100865347+supriyacode17@users.noreply.github.com>
2023-02-15 21:04:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+var randomNumber1= Math.floor(Math.random() *6)+1; //1-6
+// console.log(randomNumber1)
+
+var randomDiceImage="dice"+ randomNumber1+".png"; //dice1.png-dice6.png
+
+var randomImageSource="images/"+ randomDiceImage; //images/dice1.png-images/dice6.png
+
+var image1= document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSource);
+
+// second image
+
+var randomNumber2 =Math.floor(Math.random()*6)+1;
+
+var randomDiceImage1="dice"+randomNumber2+".png";
+
+var randomImageSource2 ="images/"+randomDiceImage1;
+
+var image2= document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",randomImageSource2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Wins!"
+}
+else if(randomNumber1<randomNumber2)
+{
+    document.querySelector("h1").innerHTML="Player 2 Wins!"
+}
+else{
+    document.querySelector("h1").innerHTML="Match Drawn Try Again once!"
+}
